3966fdaf39e26b661206c481b47dffe163db7608
Mike McClenaghan <mike@sideline.ca>
2020-04-06 15:26:50 -0600

    Got basic drag and drop working

--- main.js
+++ main.js
@@ -1,14 +1,65 @@
 window.Event = new Vue();
 
+Vue.component('lineup', {
+  template: `
+    <div>
+      <div class='forwards'>
+        <player id='lineup-1'></player>
+        <player id='lineup-2'></player>
+        <player id='lineup-3'></player>
+        <player id='lineup-4'></player>
+      </div>
+      <div class='defense'>
+        <player id='lineup-5'></player>
+        <player id='lineup-6'></player>
+        <player id='lineup-7'></player>
+      </div>
+    </div>
+  `
+});
+
+Vue.component('roster', {
+  template: `
+    <div>
+      <roster-player id="card-1" draggable='true'>Sam</roster-player>
+      <roster-player id="card-2" draggable='true'>Keaton</roster-player>
+      <roster-player id="card-3" draggable='true'>Nik</roster-player>
+    </div>
+  `
+});
+
+Vue.component('roster-player', {
+  template: `
+    <div :id='id' :draggable='draggable' @dragstart='dragStart' @dragend='dragEnd' @dragover.stop>
+      <slot/>
+    </div>
+  `,
+  props: ['id', 'draggable'],
+  methods: {
+    dragStart: e => {
+      const target = e.target;
+      e.dataTransfer.setData('card_id', target.id);
+      setTimeout(() => {
+        target.style.display = 'none';
+      }, 0);
+    },
+    dragEnd: e => {
+      const target = e.target;
+      target.style.display = 'block';
+    }
+  }
+
+});
+
 Vue.component('player', {
   template: `
-  <a @click='toggle' class='player button is-rounded is-large' :class="[isActive ? 'is-success' : 'is-danger']">
+  <a @click='toggle' @dragover.prevent @drop.prevent='drop' class='player button is-rounded is-large' :class="[isActive ? 'is-success' : 'is-danger']">
     <span class="player-number">{{ number }}</span>
     <span class="player-name"><slot></slot></span>
     <span class="player-time">{{minutes}}:{{seconds}}</span>
   </a>
   `,
-  props: ['number'],
+  props: ['number', 'id'],
   data() {
     return {
       isActive: false,
@@ -27,6 +78,11 @@ Vue.component('player', {
     },
     padTime(num){
       return (num < 10 ? '0' : '') + num.toString();
+    },
+    drop: e => {
+      const card_id = e.dataTransfer.getData('card_id');
+      const card = document.getElementById(card_id);
+      e.target.appendChild(card);
     }
   },
   computed: {
