364354e254164eb1b4d9a2560ec83f73b69256d4
Mike McClenaghan <mike@sideline.ca>
2020-04-06 18:04:41 -0600

    Rearranged components to make it easier to always be dragging player objects

--- main.js
+++ main.js
@@ -2,64 +2,39 @@ window.Event = new Vue();
 
 Vue.component('lineup', {
   template: `
-    <div>
-      <div class='forwards'>
-        <player id='lineup-1'></player>
-        <player id='lineup-2'></player>
-        <player id='lineup-3'></player>
-        <player id='lineup-4'></player>
-      </div>
-      <div class='defense'>
-        <player id='lineup-5'></player>
-        <player id='lineup-6'></player>
-        <player id='lineup-7'></player>
-      </div>
+    <div @dragover.prevent @drop.prevent='drop' class='lineup'>
+      <div class='forwards'/>
+      <div class='defense'/>
     </div>
-  `
+  `,
+  methods: {
+    drop: e => {
+      const player_id = e.dataTransfer.getData('player_id');
+      const player = document.getElementById(player_id);
+      e.target.appendChild(player);
+    }
+  }
 });
 
 Vue.component('roster', {
   template: `
-    <div>
-      <roster-player id="card-1" draggable='true'>Sam</roster-player>
-      <roster-player id="card-2" draggable='true'>Keaton</roster-player>
-      <roster-player id="card-3" draggable='true'>Nik</roster-player>
+    <div class="roster">
+      <player id="player-1" number="5" draggable='true'>Sam</player>
+      <player id="player-2" number="6" draggable='true'>Keaton</player>
+      <player id="player-3" number="8" draggable='true'>Nik</player>
     </div>
   `
 });
 
-Vue.component('roster-player', {
-  template: `
-    <div :id='id' :draggable='draggable' @dragstart='dragStart' @dragend='dragEnd' @dragover.stop>
-      <slot/>
-    </div>
-  `,
-  props: ['id', 'draggable'],
-  methods: {
-    dragStart: e => {
-      const target = e.target;
-      e.dataTransfer.setData('card_id', target.id);
-      setTimeout(() => {
-        target.style.display = 'none';
-      }, 0);
-    },
-    dragEnd: e => {
-      const target = e.target;
-      target.style.display = 'block';
-    }
-  }
-
-});
-
 Vue.component('player', {
   template: `
-  <a @click='toggle' @dragover.prevent @drop.prevent='drop' class='player button is-rounded is-large' :class="[isActive ? 'is-success' : 'is-danger']">
+  <a :id='id' :draggable='draggable' @dragstart='dragStart' @dragend='dragEnd' @dragover.stop @click='toggle' class='player button is-rounded is-large' :class="[activeClass]">
     <span class="player-number">{{ number }}</span>
     <span class="player-name"><slot></slot></span>
     <span class="player-time">{{minutes}}:{{seconds}}</span>
   </a>
   `,
-  props: ['number', 'id'],
+  props: ['number', 'id', 'draggable'],
   data() {
     return {
       isActive: false,
@@ -79,12 +54,18 @@ Vue.component('player', {
     padTime(num){
       return (num < 10 ? '0' : '') + num.toString();
     },
-    drop: e => {
-      const card_id = e.dataTransfer.getData('card_id');
-      const card = document.getElementById(card_id);
-      e.target.appendChild(card);
+    dragStart: e => {
+      const target = e.target;
+      e.dataTransfer.setData('player_id', target.id);
+      setTimeout(() => {
+        target.style.display = 'none';
+      }, 0);
+    },
+    dragEnd: e => {
+      const target = e.target;
+      target.style.display = 'inherit';
     }
-  },
+ },
   computed: {
     minutes: function() {
       const minutes = Math.floor(this.time / 60);
@@ -93,6 +74,13 @@ Vue.component('player', {
     seconds: function() {
       const seconds = this.time % 60;
       return this.padTime(seconds);
+    },
+    activeClass: function() {
+      if (this.isActive) {
+        return 'is-success';
+      } else {
+        return 'is-danger';
+      }
     }
   }
 })
